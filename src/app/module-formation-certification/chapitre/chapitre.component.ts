d536bb09520eb70c772c62ec8c6dc8a76bf77b96
EmileNapon <emilenapon0@gmail.com>
2024-12-27 16:08:37 +0000

    Initial commit

--- src/app/module-formation-certification/chapitre/chapitre.component.ts
+++ src/app/module-formation-certification/chapitre/chapitre.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 import { ActivatedRoute, Router } from '@angular/router';
 import { ChapitreService } from './chapitre-service/chapitre.service';
-
+import DOMPurify from 'dompurify';
 @Component({
   selector: 'app-chapitre',
   templateUrl: './chapitre.component.html',
@@ -25,7 +25,6 @@ export class ChapitreComponent implements OnInit {
     // Récupérer l'ID du domaine à partir de l'URL
     this.chapitreId = this.router.snapshot.paramMap.get('chapitreId');
     this.loadChapitres();
-    this.loadContenu()
   }
 
   loadChapitres(): void {
@@ -54,9 +53,37 @@ export class ChapitreComponent implements OnInit {
 
   filterData(): void {
     if (this.chapitreId) {  
-      this.chapitreFiltres = this.chapitre.filter(chapitre => chapitre.cours == this.chapitreId);
-      this.sectionFiltres = this.section.filter(section => this.chapitreFiltres.some(chapitre =>chapitre.id==section.chapitre));
-      this.ContenusFiltres = this.Contenus.filter(contenu => this.sectionFiltres.some(section=>section.id==contenu.section));
+      this.chapitreFiltres = this.chapitre.filter(chapitre => chapitre.cours == this.chapitreId)
+      .map(chapit => {
+        // Supprimer les balises <p> et </p> avant la sanitisation
+        const titre = chapit.titre.replace(/<\/?p>/g, '');
+        const id = chapit.id
+        return {
+          titre: DOMPurify.sanitize(titre), id:id
+        };
+      });
+      this.sectionFiltres = this.section.filter(section => this.chapitreFiltres.some(chapitre =>chapitre.id==section.chapitre))
+      .map(section => {
+        // Supprimer les balises <p> et </p> avant la sanitisation
+        const nom_section = section.nom_section.replace(/<\/?p>/g, '');
+        const id = section.id;
+        const chapitre = section.chapitre
+        return {
+          nom_section: DOMPurify.sanitize(nom_section), id:id, chapitre: chapitre
+        };
+      });
+      console.log('~~~~~~~~~~~~~~~~~~~',this.sectionFiltres)
+      this.ContenusFiltres = this.Contenus.filter(contenu => this.sectionFiltres.some(section=>section.id==contenu.section))
+      .map(contenu => {
+        // Supprimer les balises <p> et </p> avant la sanitisation
+        const description = contenu.description.replace(/<\/?p>/g, '');
+        const id = contenu.id;
+        const section = contenu.id
+        return {
+          description: DOMPurify.sanitize(description), id:id, section:section
+        };
+      });
+
     }
   }
 
